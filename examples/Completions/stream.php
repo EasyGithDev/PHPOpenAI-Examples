b16b6bd0df36830af5f4ddb17e50242c2084d3ac
easygithdev <easygithdev@gmail.com>
2023-03-25 15:09:14 +0100

    first example for stream

---
+++ examples/Completions/stream.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+use EasyGithDev\PHPOpenAI\OpenAIApi;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$apiKey = getenv('OPENAI_API_KEY');
+
+(new OpenAIApi($apiKey))->Completion()->create(
+    model: "text-davinci-003",
+    prompt: "Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
+    temperature: 0.3,
+    max_tokens: 100,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stream: true
+);
