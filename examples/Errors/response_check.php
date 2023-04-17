fea6e13c27bb8413c2ea3710f15463200e89e78b
easygithdev <easygithdev@gmail.com>
2023-04-17 13:32:12 +0200

    using validator builder

--- examples/Errors/response_check.php
+++ examples/Errors/response_check.php
@@ -2,23 +2,9 @@
 
 use EasyGithDev\PHPOpenAI\Helpers\ModelEnum;
 use EasyGithDev\PHPOpenAI\OpenAIClient;
-use EasyGithDev\PHPOpenAI\Validators\StatusValidator;
+use EasyGithDev\PHPOpenAI\Validators\ValidatorBuilder;
 
 require __DIR__ . '/../../vendor/autoload.php';
-
-/*
-
-// Depreciate, it will removed in the next step
-
-$response = (new OpenAIClient('BAD KEY'))->Completion()->create(
-    ModelEnum::TEXT_DAVINCI_003,
-    "Say this is a test",
-)->getResponse();
-
-if (!$response->isStatusOk() or !$response->isContentTypeOk()) {
-    echo $response->getBody();
-}
-*/
 
 $handler = (new OpenAIClient('BAD KEY'))->Completion()->create(
     ModelEnum::TEXT_DAVINCI_003,
@@ -28,6 +14,9 @@ $handler = (new OpenAIClient('BAD KEY'))->Completion()->create(
 $response = $handler->getResponse();
 $contentTypeValidator = $handler->getContentTypeValidator();
 
-if (!(new StatusValidator($response))->validate() or !(new $contentTypeValidator($response))->validate()) {
+if (
+    !ValidatorBuilder::create('status', $response)->validate() or
+    !ValidatorBuilder::create($contentTypeValidator, $response)->validate()
+) {
     echo $response->getBody();
 }
